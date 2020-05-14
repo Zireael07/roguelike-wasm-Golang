9c323d10313dc0eb384782e7328843a0540e5649
Zireael07 <zi@mail.com>
2020-05-14 15:17:06 +0200

    new: Lua now spawns the npc based on a data table

--- wasm/hello.lua
+++ wasm/hello.lua
@@ -1,3 +1,15 @@
+----------------------------
+-- DATA
+
+data_cop = { 
+   ["position"] = {3,3}, ["renderable"] = {{0,0,255,255}, "c"}, ["stats"] = { hp = 10, power =2 }, ["name"] = "cop"
+}
+
+
+
+------------------------------
+--FUNCTIONS
+
 print("hello WASM from lua")
 
 -- very basic function to dump table to readable format
@@ -14,60 +26,49 @@ function dump(o)
    end
 end
 
+function spawn_npc(data)
+   --test Go interop 
+   ent = Ent()
+   --print(ent)
 
-data_all = { 
-    ["position"] = {2,2}, ["renderable"] = {{0,0,255,255}, "c"}, ["stats"] = { hp = 10, power =2 }, ["name"] = "cop"
-}
+   ent:SetupComponentsMap()
+   --add components from Lua
+   pos = Position()
+   -- lua indexes from 1 not 0 as Python does
+   pos.Pos.X = data["position"][1]
+   pos.Pos.Y = data["position"][2]
 
---test Go interop 
-ent = Ent()
---print(ent)
+   render = Renderable()
+   render.Color.R = data["renderable"][1][1]
+   render.Color.G = data["renderable"][1][2]
+   render.Color.B = data["renderable"][1][3]
+   render.Color.A = data["renderable"][1][4]
+   render.Glyph = string.byte(data['renderable'][2], 1) -- 99 -- 'c'
 
-ent:SetupComponentsMap()
---add components from Lua
-pos = Position()
-pos.Pos.X = 3
-pos.Pos.Y = 3
--- print(pos.Pos.X)
--- print(pos.Pos.Y)
+   -- print("Glyph: ", render.Glyph)
+   name = Name()
+   name.Name = data["name"]
+   --print("Name", name.Name)
 
-render = Renderable()
-render.Color.R = 0
-render.Color.G = 0
-render.Color.B = 255
-render.Color.A = 255
-render.Glyph = 99 -- 'c'
+   stats = Stats()
+   stats.Hp = data["stats"]["hp"]
+   stats.Max_hp = data["stats"]["hp"]
+   stats.Power = data["stats"]["power"]
 
--- print("Glyph: ", render.Glyph)
-name = Name()
-name.Name = "Cop"
---print("Name", name.Name)
+   -- these two aren't defined in data because no need to
+   block = Blocker()
+   npc = NPC()
 
-block = Blocker()
-npc = NPC()
-stats = Stats()
-stats.Hp = 10
-stats.Max_hp = 10
-stats.Power = 2
+   --the minus is the unary operator, dereferencing pointers
+   ent:AddComponent("position", -pos)
+   ent:AddComponent("renderable", -render)
+   ent:AddComponent("name", -name)
+   ent:AddComponent("blocker", -block)
+   ent:AddComponent("NPC", -npc)
+   ent:AddComponent("stats", -stats)
 
---the minus is the unary operator, dereferencing pointers
-ent:AddComponent("position", -pos)
-ent:AddComponent("renderable", -render)
-ent:AddComponent("name", -name)
-ent:AddComponent("blocker", -block)
-ent:AddComponent("NPC", -npc)
-ent:AddComponent("stats", -stats)
+   entities:add(ent)
+end
 
-entities:add(ent)
-
---(calling functions)
---ent:AddComponentLua("TestComp")
-
---test Go interop (passing data)
--- data = {
---    name = "Test"
--- }
-
-
-
- --print(dump(data))
+-- do stuff!
+spawn_npc(data_cop)
